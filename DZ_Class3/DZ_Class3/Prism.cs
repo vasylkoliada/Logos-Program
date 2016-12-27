f0ea6639bfc1b7c9b94c6beec42a9eb92fe30431
vasylkoliada <vasyl.koliada@gmail.com>
2016-12-27 17:06:57 +0200

    27.12.2016

---
+++ DZ_Class3/DZ_Class3/Prism.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_Class3
+{
+   public   class Prism
+   {
+       private double size;
+
+       public double GetSize()
+       {
+           return size;
+       }
+       public void SetSize(double _size)
+       {
+           size = _size;
+       }
+
+       private double height;
+
+       public double GetHeight()
+       {
+           return height;
+       }
+
+       public void SetHeight(double _height)
+       {
+           height = _height;
+       }
+
+       private int countMargin;
+
+       public int GetMargin()
+       {
+           return countMargin;
+       }
+
+       public void SetMargin(int _countMargin)
+       {
+           countMargin = _countMargin;
+       }
+
+       private double lenght;
+
+       public double GetLenght()
+       {
+           return lenght;
+       }
+
+       public void SetLenght(double _lenght)
+       {
+           lenght = _lenght;
+       }
+
+
+       private double area;
+
+       public double GetArea()
+       {
+           return area;
+       }
+
+       public void SetArea(double _area)
+       {
+           area = _area;
+       }
+      
+
+        public Prism(double _size, double _height , int _countMargin , double _lenght , double _area )
+        {
+            size = _size;
+            height = _height;
+            countMargin = _countMargin;
+            lenght = _lenght;
+            area = _area;
+        }
+
+
+    }
+}
---
+++ DZ_Class3/DZ_Class3/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_Class3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prism p = new Prism(34.2, 12, 34, 13.5, 20.4);
+            //p.SetSize(567);
+            //Console.WriteLine(p.GetSize());
+            bool a = false;
+            while (!a)
+            {
+                Console.WriteLine(
+                    "Зробiть вибiр:\n 0 - вихiд; 1 - показати висоту призми;\n2 -  показати кiлькiсть бокових граней; 3- показати площу основи;\n4 - показати довжину сторони призми; 5 - показати об'єм призми;\n6 - встановити висоту призми; 7 - встановити довжину сторони призми;\n8 - встановити кiлькiсть граней; 9 - задати площу основи\n >> ");
+                int k = int.Parse(Console.ReadLine());
+                switch (k)
+                {
+                    case 0:
+                        a = true;  return;
+                    case 1:
+                        Console.WriteLine(p.GetHeight());
+                        break;
+                    case 2:
+                        Console.WriteLine(p.GetMargin());
+                        break;
+                    case 3:
+                        Console.WriteLine(p.GetArea());
+                        break;
+                    case 4:
+                        Console.WriteLine(p.GetLenght());
+                        break;
+                    case 5:
+                        Console.WriteLine(p.GetSize());
+                        break;
+                    case 6:
+                        p.SetHeight(double.Parse(Console.ReadLine()));
+                        Console.WriteLine(p.GetHeight());
+                        break;
+                    case 7:
+                        p.SetLenght(double.Parse(Console.ReadLine()));
+                        Console.WriteLine(p.GetLenght());
+                        break;
+                    case 8:
+                        p.SetMargin(int.Parse(Console.ReadLine()));
+                        Console.WriteLine(p.GetMargin());
+                        break;
+                    case 9:
+                        p.SetArea(double.Parse(Console.ReadLine()));
+                        Console.WriteLine(p.GetArea());
+                        break;
+                    default:
+                        Console.WriteLine("Не правильне число");
+                        a = true; return;
+                }
+            }
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
